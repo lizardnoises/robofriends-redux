0f32c91f92181f99040ab646ff26a30e9c7ea388
Sean Moore <sean.m.moore@gmail.com>
2021-04-30 15:22:18 -0700

    Update card component to use robot data properties

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './Card';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+    <>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
